e247e0bdcf85cac55d7cef67518452ee112b4c98
Timothy Watson <timwatson241@gmail.com>
2023-04-10 07:41:08 -0600

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, Skeleton } from "@chakra-ui/react";
+import { CardBody, SkeletonText } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
